96f257d28cedadb8bdea2def9a69eae1caaa3696
tomasberretta <tomas.berretta@ing.austral.edu.ar>
2022-05-03 16:43:37 -0300

    Initial commit

---
+++ src/main/java/com/tombtomb/jjbackend/JjBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tombtomb.jjbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tombtomb/jjbackend/JjBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tombtomb.jjbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
