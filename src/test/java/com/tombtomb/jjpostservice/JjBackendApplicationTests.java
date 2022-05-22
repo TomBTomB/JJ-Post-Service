7dfd804085626925a635861cdc2088c998f2642c
TomasBruno1 <tomas.bruno@ing.austral.edu.ar>
2022-05-22 12:32:23 -0300

    Workflow to publish image
    Co-authored-by: tomasberretta <tomas.berretta@ing.austral.edu.ar>

--- src/test/java/com/tombtomb/jjpostservice/JjBackendApplicationTests.java
+++ src/test/java/com/tombtomb/jjpostservice/JjBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JjBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
