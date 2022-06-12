6e654149c89ae45530493e36b708279abbdf8e0f
tomasberretta <tomas.berretta@ing.austral.edu.ar>
2022-06-12 16:56:56 -0300

    fix datadog

--- src/main/java/com/tombtomb/jjpostservice/controller/UserController.java
+++ src/main/java/com/tombtomb/jjpostservice/controller/UserController.java
@@ -3,12 +3,14 @@ package com.tombtomb.jjpostservice.controller;
 import com.tombtomb.jjpostservice.service.UserService;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
+import org.springframework.metrics.annotation.Timed;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/user")
+@Timed("user_controller_time")
 public class UserController {
     private final UserService userService;
 
@@ -17,18 +19,21 @@ public class UserController {
     }
 
     @GetMapping
+    @Timed
     public ResponseEntity<?> getLoggedUser() {
         val user = userService.getLoggedUser();
         return ResponseEntity.ok(user);
     }
 
     @GetMapping("/{id}")
+    @Timed
     public ResponseEntity<?> getUser(@PathVariable UUID id) {
         val user = userService.getUser(id);
         return ResponseEntity.ok(user);
     }
 
     @GetMapping("/search/{searchString}")
+    @Timed
     public ResponseEntity<?> getUser(@PathVariable String searchString) {
         val users = userService.searchUser(searchString);
         return ResponseEntity.ok(users);
